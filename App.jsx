a7caf8464f771454702cc58456aa6782d1873e09
BhavaAkshaya5 <bhavaakshaya2006@gmail.com>
2026-07-14 21:06:46 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import "./Assets/css/style.css";
 import { HashRouter } from "react-router-dom";
-import AppRoutes from "./Routes/AppRoutes";
 
 function App() {
   return (
